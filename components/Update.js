31ad9ecc85f7f078f555f01be04d9553db0c7f34
Dedinfosec <72137870+0xdedinfosec@users.noreply.github.com>
2021-05-23 12:54:19 +0530

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -1,12 +1,12 @@
 document.getElementById("Recent-Updates").innerHTML = `<li>
-<a href="/posts/htb-scriptkiddie/">Hackthebox ScriptKiddie writeup</a>
+<a href="/posts/htb-pivotapi/">Hackthebox Pivotapi writeup</a>
+</li>
+<li>
+<a href="/posts/htb-love/">Hackthebox Love writeup</a>
 </li>
 <li>
 <a href="/posts/htb-sink/">Hackthebox Sink writeup</a>
 </li>
 <li>
 <a href="/posts/htb-tenet/">Hackthebox Tenet writeup</a>
-</li>
-<li>
-<a href="/posts/htb-cereal/">Hackthebox testing writeup</a>
 </li>`
