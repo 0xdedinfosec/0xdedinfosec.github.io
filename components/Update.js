1540ff989f028709701cfcabf71e399499b18782
Dedinfosec <72137870+0xdedinfosec@users.noreply.github.com>
2021-05-23 12:49:41 +0530

    Add files via upload

---
+++ components/Update.js
@@ -0,0 +1,12 @@
+document.getElementById("Recent-Updates").innerHTML = `<li>
+<a href="/posts/htb-scriptkiddie/">Hackthebox ScriptKiddie writeup</a>
+</li>
+<li>
+<a href="/posts/htb-sink/">Hackthebox Sink writeup</a>
+</li>
+<li>
+<a href="/posts/htb-tenet/">Hackthebox Tenet writeup</a>
+</li>
+<li>
+<a href="/posts/htb-cereal/">Hackthebox testing writeup</a>
+</li>`
