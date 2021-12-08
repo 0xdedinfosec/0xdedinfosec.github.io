58bfac269156b7b73d7201af6c08e82ab9bf44cd
Dedinfosec <72137870+0xdedinfosec@users.noreply.github.com>
2021-12-08 09:43:06 +0530

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -1,11 +1,14 @@
-// import {hackthebox_arr} from "./Variable.js";
+import {hackthebox_arr} from "./Variable.js";
 
-// let random1 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-// let random2 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-// let random3 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-// let random4 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+let random1 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+let random2 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+let random3 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+let random4 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
 
-// document.getElementById("Recent-Updates").innerHTML = `<li>
+document.getElementById("Recent-Updates").innerHTML = "<li>
+  <a href="https://0xdedinfosec.vercel.app/">Check Out My New Website</a>
+</li>"
+// `<li>
 // <a href="/posts/htb-${random1.toLowerCase()}/">Hackthebox ${random1} writeup</a>
 // </li>
 // <li>
