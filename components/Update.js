6f359c8fde670bde727d83e1ce055307376f0f43
Dedinfosec <72137870+0xdedinfosec@users.noreply.github.com>
2021-12-08 09:47:02 +0530

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -5,10 +5,10 @@ let random2 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
 let random3 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
 let random4 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
 
-document.getElementById("Recent-Updates").innerHTML = "<li>
+document.getElementById("Recent-Updates").innerHTML = '<li>
   <a href="https://0xdedinfosec.vercel.app/">Check Out My New Website</a>
-</li>"
-// `<li>
+</li>'
+// <li>
 // <a href="/posts/htb-${random1.toLowerCase()}/">Hackthebox ${random1} writeup</a>
 // </li>
 // <li>
@@ -19,4 +19,4 @@ document.getElementById("Recent-Updates").innerHTML = "<li>
 // </li>
 // <li>
 // <a href="/posts/htb-${random4.toLowerCase()}/">Hackthebox ${random4} writeup</a>
-// </li>`
+// </li>
