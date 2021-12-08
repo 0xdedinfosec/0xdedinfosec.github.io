a2d61fb9e2546f53a5ec3ca47becd7731f7c86cc
Dedinfosec <72137870+0xdedinfosec@users.noreply.github.com>
2021-12-08 09:40:08 +0530

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -1,19 +1,19 @@
-import {hackthebox_arr} from "./Variable.js";
+// import {hackthebox_arr} from "./Variable.js";
 
-let random1 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-let random2 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-let random3 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
-let random4 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+// let random1 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+// let random2 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+// let random3 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
+// let random4 = hackthebox_arr[Math.floor(Math.random() * hackthebox_arr.length)];
 
-document.getElementById("Recent-Updates").innerHTML = `<li>
-<a href="/posts/htb-${random1.toLowerCase()}/">Hackthebox ${random1} writeup</a>
-</li>
-<li>
-<a href="/posts/htb-${random2.toLowerCase()}/">Hackthebox ${random2} writeup</a>
-</li>
-<li>
-<a href="/posts/htb-${random3.toLowerCase()}/">Hackthebox ${random3} writeup</a>
-</li>
-<li>
-<a href="/posts/htb-${random4.toLowerCase()}/">Hackthebox ${random4} writeup</a>
-</li>`
+// document.getElementById("Recent-Updates").innerHTML = `<li>
+// <a href="/posts/htb-${random1.toLowerCase()}/">Hackthebox ${random1} writeup</a>
+// </li>
+// <li>
+// <a href="/posts/htb-${random2.toLowerCase()}/">Hackthebox ${random2} writeup</a>
+// </li>
+// <li>
+// <a href="/posts/htb-${random3.toLowerCase()}/">Hackthebox ${random3} writeup</a>
+// </li>
+// <li>
+// <a href="/posts/htb-${random4.toLowerCase()}/">Hackthebox ${random4} writeup</a>
+// </li>`
